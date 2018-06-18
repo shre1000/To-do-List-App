d7dc8b9331d4ef7f9483238195f127ecf740b02c
Shre1000 <31860419+shre1000@users.noreply.github.com>
2018-06-18 14:04:25 -0500

    Add files via upload

---
+++ MyNotesApp/MyNotesApp/MyNotesApp.h
@@ -0,0 +1,19 @@
+//
+//  MyNotesApp.h
+//  MyNotesApp
+//
+//  Created by Kokam on 6/12/18.
+//  Copyright © 2018 Kokam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyNotesApp.
+FOUNDATION_EXPORT double MyNotesAppVersionNumber;
+
+//! Project version string for MyNotesApp.
+FOUNDATION_EXPORT const unsigned char MyNotesAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyNotesApp/PublicHeader.h>
+
+
